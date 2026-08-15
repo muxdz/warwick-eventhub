6ac285484b4717a4b0343da4af6d45cc1fe75def
Muadz <muadz.uk@gmail.com>
2026-08-15 18:31:11 +0100

    chore: cleaned up schema and md file

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -1,26 +1,29 @@
 CREATE TABLE users (
-    id BIGINT GENERATED ALWAYS AS IDENTITY  primary key,
-    user_name VARCHAR(50)NOT NULL,
+    id BIGINT GENERATED ALWAYS AS IDENTITY primary key,
+    user_name VARCHAR(50) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
     created_at TIMESTAMPTZ NOT NULL default NOW(),
     password_hash TEXT NOT NULL
 );
 
 CREATE TABLE societies (
-    id BIGINT GENERATED ALWAYS AS IDENTITY  primary key,
+    id BIGINT GENERATED ALWAYS AS IDENTITY primary key,
     society_name VARCHAR(100) NOT NULL UNIQUE,
     created_at TIMESTAMPTZ NOT NULL default NOW()
 );
 
 CREATE TABLE events (
-    id BIGINT GENERATED ALWAYS AS IDENTITY  primary key,
+    id BIGINT GENERATED ALWAYS AS IDENTITY primary key,
     event_title VARCHAR(200) NOT NULL,
     event_location VARCHAR(200) NOT NULL,
+    event_time TIMESTAMPTZ NOT NULL,
+    description VARCHAR(500), 
     society_id BIGINT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL default NOW(),
 
-    FOREIGN KEY (society_id) 
-        REFERENCES societies (id)
+
+    FOREIGN KEY (society_id)
+        REFERENCES societies (id) ON DELETE CASCADE
 );
 
 CREATE TABLE memberships (
@@ -29,10 +32,10 @@ CREATE TABLE memberships (
     society_id BIGINT NOT NULL,
 
     FOREIGN KEY (user_id) 
-        REFERENCES users (id),
+        REFERENCES users (id) ON DELETE CASCADE,
 
     FOREIGN KEY (society_id) 
-        REFERENCES societies (id)
+        REFERENCES societies (id) ON DELETE CASCADE
 );
 
 CREATE TABLE bookmarks (
@@ -41,9 +44,9 @@ CREATE TABLE bookmarks (
     event_id BIGINT NOT NULL,
 
     FOREIGN KEY (user_id) 
-        REFERENCES users (id),
+        REFERENCES users (id) ON DELETE CASCADE,
 
     FOREIGN KEY (event_id) 
-        REFERENCES events (id)
+        REFERENCES events (id) ON DELETE CASCADE
 );
 
