a04eea482d8704f57708ad23344c4aa4ac7b0248
Muadz <muadz.uk@gmail.com>
2026-08-17 15:27:27 +0100

    docs: updated store and schemas to match database

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -8,6 +8,15 @@ class EventCreate(BaseModel):
     description: str | None = None
     society_id: int
     created_by_user_id: int
+    image_key: str | None = None
+
+class EventUpdate(BaseModel):
+    event_title: str | None = None
+    event_location: str | None = None
+    start_time: str | None = None
+    end_time: str | None = None
+    description: str | None = None
+    image_key: str | None = None
 
 class Event(EventCreate):
     id: int
--- backend/app/store.py
+++ backend/app/store.py
@@ -2,23 +2,30 @@ from app.schemas import Event
 
 event1 = Event(
     id = 1,
-    title = "Welcome Event",
-    society = "Rowing Society",
-    location = "Oculus"
+    event_title = "Welcome Event",
+    event_location = "Oculus",
+    start_time = "2023-05-01 10:00:00",
+    end_time = "2023-05-01 12:00:00",
+    society_id = 1,
+    created_by_user_id = 1
 )
 
 event2 = Event(
     id = 2,
-    title = "Meet the Exec Event",
-    society = "Badminton Society",
-    location = "Oculus"
+    event_title = "Welcome Event 2 ",
+    event_location = "FAB",
+    start_time = "2024-05-01 10:00:00",
+    society_id = 1,
+    created_by_user_id = 1
 )
 
 event3 = Event(
     id = 3,
-    title = "Painting Event",
-    society = "Painting Society",
-    location = "FAB"
+    event_title = "Welcome Event",
+        event_location = "Oculus",
+        start_time = "2025-05-01 10:00:00",
+        society_id = 1,
+        created_by_user_id = 1
 )
 
 events = [event1, event2, event3]
