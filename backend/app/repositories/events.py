7eb1975f68b3bfebb35d38234eb27888d5e5a99f
Muadz <muadz.uk@gmail.com>
2026-08-17 14:38:04 +0100

    feat: converted POST to use postgres method

--- backend/app/repositories/events.py
+++ backend/app/repositories/events.py
@@ -28,3 +28,41 @@ def get_event_by_id(event_id: int):
             )
 
             return cur.fetchone()
+
+def create_event(event_data):   
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                INSERT INTO events (
+                    event_title,
+                    event_location,
+                    start_time, 
+                    end_time,
+                    description,
+                    society_id,
+                    created_by_user_id
+                )
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+                RETURNING 
+                    id,
+                    event_title,
+                    event_location,
+                    start_time, 
+                    end_time,
+                    description,
+                    society_id,
+                    created_by_user_id;
+                """,
+                (
+                    event_data.title,
+                    event_data.society,
+                    event_data.location,
+                    event_data.start_time,
+                    event_data.end_time
+                )
+            )
+
+            return cur.fetchone()
+
+        
