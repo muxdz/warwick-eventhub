a6cc939e723a012f04d186294b85e8c7e6c0bc09
Muadz <muadz.uk@gmail.com>
2026-08-17 17:41:32 +0100

    feat: turned python array tests to a test database

--- backend/tests/test_events.py
+++ backend/tests/test_events.py
@@ -5,32 +5,78 @@ from app.main import app
 from app.schemas import Event
 from app import store
 
+from app.database import get_connection
+
 client = TestClient(app)
+
+def seed_event(
+    cur,
+    title,
+    location,
+    society_id,
+    created_by_user_id,
+):
+    cur.execute(
+        """
+        INSERT INTO events (
+            event_title,
+            event_location,
+            start_time,
+            end_time,
+            description,
+            society_id,
+            created_by_user_id
+        )
+        VALUES (%s, %s, %s, %s, %s, %s, %s);
+        """,
+        (
+            title,
+            location,
+            "2026-10-10 10:00:00+01",
+            "2026-10-10 12:00:00+01",
+            "Test description",
+            society_id,
+            created_by_user_id
+        )
+    )
+
+def seed_society(cur, society_name):
+    cur.execute(
+        """
+        INSERT INTO societies (society_name)
+        VALUES (%s);
+        """,
+        (society_name,)
+    )
+
+def seed_user(cur, user_name, email):
+    cur.execute(
+        """
+        INSERT INTO users (user_name, email, password_hash)
+        VALUES (%s, %s, 'test_hash');
+        """,
+        (user_name, email)
+    )
 
 @pytest.fixture(autouse=True)
 def reset_events():
-    store.events.clear()
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                """
+                TRUNCATE TABLE events, societies, users 
+                RESTART IDENTITY CASCADE;
+                """
+            )
 
-    store.events.extend([
-        Event(
-            id = 1,
-            title = "Welcome Event",
-            society = "Society",
-            location = "Oculus"
-        ),
-        Event(
-            id = 2,
-            title = "Meet the Exec Event",
-            society = "Badminton Society",
-            location = "Oculus"
-        ),
-        Event(
-            id = 3,
-            title = "Painting Event",
-            society = "Painting Society",
-            location = "FAB"
-        )
-    ])
+            seed_user(cur, "Alice", "alice@example.com")
+            seed_user(cur, "Bob", "bob@example.com")
+
+            seed_society(cur, "Cloud Society")
+            seed_society(cur, "Engineering Society")
+
+            seed_event(cur, "Python", "Rootes", 1, 1)
+            seed_event(cur, "AWS", "FAB", 2, 2)
 
 def test_health():
     response = client.get("/health")
