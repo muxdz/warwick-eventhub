d3bf52d809f262d1d6a1b040ba50a5450951dcf2
Muadz <muadz.uk@gmail.com>
2026-08-19 15:13:09 +0100

    chore: translated schema.sql tables into migration001

---
+++ backend/migrations/versions/35598e6f1414_create_initial_eventhub_schema.py
@@ -0,0 +1,130 @@
+"""create initial eventhub schema
+
+Revision ID: 35598e6f1414
+Revises: 
+Create Date: 2026-08-19 14:20:23.715495
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '35598e6f1414'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("user_name", sa.String(50), nullable=False),
+        sa.Column("email", sa.String(255), nullable=False, unique=True),
+        sa.Column("created_at", sa.TIMESTAMP, nullable=False, server_default=sa.func.now()),
+        sa.Column("password_hash", sa.Text, nullable=False)
+    )
+
+    op.create_table(
+        "societies",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("society_name", sa.String(100), nullable=False, unique=True),
+        sa.Column("created_at", sa.TIMESTAMP, nullable=False, server_default=sa.func.now())
+    )
+
+    op.create_table(
+        "events",
+        sa.Column("id", sa.Integer, primary_key=True),
+        sa.Column("event_title", sa.String(200), nullable=False),
+        sa.Column("event_location", sa.String(200), nullable=False),
+        sa.Column("start_time", sa.TIMESTAMP, nullable=False),
+        sa.Column("end_time", sa.TIMESTAMP),
+        sa.Column("description", sa.String(500)),
+        sa.Column("society_id", sa.Integer, nullable=False),
+        sa.Column("created_at", sa.TIMESTAMP, nullable=False, server_default=sa.func.now()),
+        sa.Column("created_by_user_id", sa.Integer, nullable=False),
+        sa.Column("image_key", sa.String(200)),
+
+        sa.CheckConstraint(
+            "end_time > start_time OR end_time IS NULL",
+            name="valid_end_time",
+        ),
+
+        sa.ForeignKeyConstraint(
+            ["society_id"],
+            ["societies.id"],
+            ondelete="CASCADE",
+            onupdate="CASCADE"
+        ),
+        sa.ForeignKeyConstraint(
+            ["created_by_user_id"],
+            ["users.id"],
+            ondelete="CASCADE",
+            onupdate="CASCADE"
+        )
+    )
+
+    op.create_table(
+        "memberships",
+        sa.Column("user_id", sa.Integer, nullable=False),
+        sa.Column("society_id", sa.Integer, nullable=False),
+
+        sa.PrimaryKeyConstraint(
+            "user_id", 
+            "society_id",
+            name="pk_memberships"
+        ),
+
+        sa.ForeignKeyConstraint(
+            ["user_id"],
+            ["users.id"],
+            ondelete="CASCADE",
+            onupdate="CASCADE"
+        ),
+        sa.ForeignKeyConstraint(
+            ["society_id"],
+            ["societies.id"],
+            ondelete="CASCADE",
+            onupdate="CASCADE"
+        ),
+
+        sa.CheckConstraint(
+            "role in ('organiser', 'member')",
+            name="valid_role",
+        ),
+    )
+
+    op.create_table(
+        "bookmarks",
+        sa.Column("user_id", sa.Integer, nullable=False),
+        sa.Column("event_id", sa.Integer, nullable=False),
+
+        sa.PrimaryKeyConstraint(
+            "user_id", 
+            "event_id",
+            name="pk_bookmarks"
+        )
+
+        sa.ForeignKeyConstraint(
+            ["user_id"],
+            ["users.id"],
+            ondelete="CASCADE",
+            onupdate="CASCADE"
+        ),
+        sa.ForeignKeyConstraint(
+            ["event_id"],
+            ["events.id"],
+            ondelete="CASCADE",
+            onupdate="CASCADE"
+        )
+
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
