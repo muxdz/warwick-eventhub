c6bfb495b5bd88c2a70716cca8d20a891fdd364d
Muadz <muadz.uk@gmail.com>
2026-08-20 13:37:11 +0100

    feat:  added POST /users so we are able to add userr

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
 from app.routers.events import events_router
+from app.routers.users import users_router
 
 app = FastAPI()
 
 app.include_router(events_router)
+app.include_router(users_router)
 
 @app.get("/health")
 def get_health():
