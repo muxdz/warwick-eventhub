85041120d2681962b816df1d22057d2126b0ba73
Muadz <muadz.uk@gmail.com>
2026-08-21 19:57:39 +0100

    feat: added authentication using fastapi security

--- backend/app/security.py
+++ backend/app/security.py
@@ -1,12 +1,17 @@
+from fastapi import HTTPException
+from fastapi.security import OAuth2PasswordBearer, Depends
+
 from pwdlib import PasswordHash
 from app.config import settings
 from datetime import datetime, timedelta, timezone
+from app.repositories import users as user_repository
 
 import jwt
 
 password_hash = PasswordHash.recommended()
 
 algorithm = "HS256"
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 secret_key = settings.jwt_secret_key
 minutes = int(settings.jwt_expire_minutes)
 
@@ -29,3 +34,30 @@ def create_access_token(user_id: int) -> str:
 
 def decode_access_token(token: str) -> dict:
     return jwt.decode(token, secret_key, algorithms=[algorithm])
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    payload = decode_access_token(token)
+
+    if not payload:
+        raise HTTPException(
+            status_code=401,
+            detail="Could not validate credentials"
+        )
+
+    user_id = payload.get("user_id")
+
+    if not user_id:
+        raise HTTPException(
+            status_code=401,
+            detail="Could not validate credentials"
+        )
+
+    user = user_repository.get_user_by_id(user_id)
+
+    if not user:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+
+    return decode_access_token(token)
