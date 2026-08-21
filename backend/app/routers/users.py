5d443e5e1d356ce6f97ff4a1b5b2c884b70a24b2
Muadz <muadz.uk@gmail.com>
2026-08-21 20:32:11 +0100

    feat: added ability to login

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -1,20 +1,23 @@
 from fastapi import APIRouter, HTTPException, Depends
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordRequestForm
 
 from app.repositories import users as user_repository
 
 from app.schemas.users import UserCreate, UserUpdate, UserResponse
 
-from app.security import hash_password, verify_password, get_current_user, create_access_token
+from app.security import hash_password, verify_password, get_current_user, create_access_token, oauth2_scheme
 
 users_router = APIRouter(tags=["users"])
 
 @users_router.get("/users/me")
-def get_me(current_user: UserResponse = Depends(get_current_user)):
+def get_me(current_user = Depends(get_current_user)):
     return current_user
 
-@users_router.get("/auth/login")
-def login_user(email: str, password: str):
+@users_router.post("/auth/login")
+def login_user(form_data: OAuth2PasswordRequestForm = Depends()):
+    email = form_data.username
+    password = form_data.password
+
     if email is None or password is None:
         raise HTTPException(
             status_code=400,
@@ -23,19 +26,18 @@ def login_user(email: str, password: str):
 
     user = user_repository.get_user_by_email(email)
 
-    if not user or user is None:
-        raise HTTPException(
-            status_code=404,
-            detail="User not found"
-        )
-
-    if not verify_password(password, user.password_hash):
+    if user is None or not verify_password(password, user["password_hash"]):
         raise HTTPException(
             status_code=401,
-            detail="Incorrect password"
+            detail="Incorrect credentials"
         )
 
-    return create_access_token(user.json()["user_id"])
+    token = create_access_token(user["id"])
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
 
 @users_router.get("/users")
 def get_all_users():
--- backend/app/security.py
+++ backend/app/security.py
@@ -11,7 +11,7 @@ import jwt
 password_hash = PasswordHash.recommended()
 
 algorithm = "HS256"
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 secret_key = settings.jwt_secret_key
 minutes = int(settings.jwt_expire_minutes)
 
@@ -37,20 +37,7 @@ def decode_access_token(token: str) -> dict:
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
     payload = decode_access_token(token)
-
-    if not payload:
-        raise HTTPException(
-            status_code=401,
-            detail="Could not validate credentials"
-        )
-
     user_id = payload.get("user_id")
-
-    if not user_id:
-        raise HTTPException(
-            status_code=401,
-            detail="Could not validate credentials"
-        )
 
     user = user_repository.get_user_by_id(user_id)
 
