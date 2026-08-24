83bfa797f628c722ac80645f7d2062d0556ce2ae
Muadz <muadz.uk@gmail.com>
2026-08-24 12:46:44 +0100

    feat: built reusable event and society cards

---
+++ frontend/data/societies.ts
@@ -0,0 +1,14 @@
+import type { Society } from "@/types/societies";
+
+export const societies: Society[] = [
+    {
+        id: 1,
+        society_name: "Computing Society",
+        created_at: new Date("2026-10-05T18:00:00+01:00"),
+    },
+    {
+        id: 2,
+        society_name: "Engineering Society",
+        created_at: new Date("2026-10-05T18:00:00+01:00"),
+    },
+];
