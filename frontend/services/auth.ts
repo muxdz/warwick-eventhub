b084c56a25864337170b44927367db448bb6991d
Muadz <muadz.uk@gmail.com>
2026-08-25 18:40:50 +0100

    fix: moved logging in users to auth file

--- frontend/services/auth.ts
+++ frontend/services/auth.ts
@@ -1,6 +1,26 @@
-export async function login(email : string, password : string) {
+export async function login(formData: URLSearchParams) {
     const reponse = await fetch (
-        `${process.env.NEXT_PUBLIC_API_URL}/auth/login`
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: formData
+        }
+    );
+
+    return reponse;
+}
+
+export async function get_current_user(token: string) {
+    const reponse = await fetch (
+        `${process.env.NEXT_PUBLIC_API_URL}/users/me`, 
+        {
+            headers: {
+                "Authorization": `Bearer ${token}`
+            }
+        }
     );
 
     return reponse.json();
