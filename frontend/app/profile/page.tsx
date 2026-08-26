41bf3123171ebc77602b60e294b32d7d78963f13
Muadz <muadz.uk@gmail.com>
2026-08-26 15:49:02 +0100

    feat: added view bookmarks button on profile

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -1,9 +1,17 @@
 import Profile from "@/components/Profile";
+import Link from "next/link";
 
 export default function Page() {
     return (
         <main>
             <Profile />
+
+            <Link 
+                href="/profile/bookmarks"
+                className="mt-8 block rounded-md bg-blue-600 px-4 py-2.5 font-medium text-white hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+            >
+                View Bookmarks
+            </Link>
         </main>
     );
 }
