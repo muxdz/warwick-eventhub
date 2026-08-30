642c3f9ce447fc1ae5006d7bea8a44da1d35c647
Muadz <muadz.uk@gmail.com>
2026-08-30 20:05:03 +0100

    feat: users are able to create societies

--- frontend/services/societies.ts
+++ frontend/services/societies.ts
@@ -1,4 +1,4 @@
-import type { Society } from "@/types/societies";
+import type { Society, SocietyCreate } from "@/types/societies";
 import { ApiError } from "./errors";
 
 export async function GetSocieties(): Promise<Society[]> {
@@ -27,6 +27,31 @@ export async function GetSociety(id: number): Promise<Society | null> {
 
         throw new ApiError(
             error.detail ?? "Failed to get society",
+            response.status
+        );
+    }
+
+    return response.json();
+}
+
+export async function CreateSociety(data: SocietyCreate, token: string): Promise<Society | null> {
+    const response = await fetch (
+        `${process.env.NEXT_PUBLIC_API_URL}/societies`,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify(data)
+        }
+    );
+
+    if (!response.ok) {
+        const error = await response.json();
+
+        throw new ApiError(
+            error.detail ?? "Failed to create society",
             response.status
         );
     }
