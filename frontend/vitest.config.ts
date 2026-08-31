a73255ee351ac62be116e2cda0d7efb60f0f59f0
Muadz <muadz.uk@gmail.com>
2026-08-31 14:32:52 +0100

    chore: updated vitest files and dependencies

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from "vitest/config";
+import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    plugins: [
+        react(),
+        tsconfigPaths()
+    ],
+
     test: {
         environment: "jsdom",
         setupFiles: ["./vitest.setup.ts"],
