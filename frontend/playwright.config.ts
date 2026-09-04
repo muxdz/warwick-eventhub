5fbdb61587028fe69cb078c28563282191b2989d
Muadz <muadz.uk@gmail.com>
2026-09-04 19:29:53 +0100

    feat: playwright testing now automatically runs the frontend setup

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -3,8 +3,14 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
     testDir: './e2e',
 
+    webServer: {
+        command: "npm run dev",
+        url: "http://localhost:3000",
+        reuseExistingServer: true,
+    },
+
     use: {
-        baseURL: 'http://localhost:3000',
+        baseURL: process.env.PLAYWRIGHT_BASE_URL ?? 'http://localhost:3000',
         trace: 'on-first-retry',
     },
 
